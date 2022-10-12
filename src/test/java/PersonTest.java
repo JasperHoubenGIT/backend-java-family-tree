f7c1ff3ba573f3649caa2e4431ba7f0f79446c34
JasperHoubenGIT <jasperhouben@gmail.com>
2022-10-12 22:42:23 +0200

    finale upload

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,8 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import javax.swing.event.ListSelectionEvent;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void addChild() {
+        //arange
+        Person parent = new Person("jasper", "Houben","man",27);
+        Person child = new Person("floris", "Houben", "man",2);
+        List<Person> children = new ArrayList<>();
+
+        //act
+        parent.addChild(parent, child);
+        List<Person> kids = parent.getChildren();
+
+        //assert
+        assertEquals(children, kids);
+        assertEquals("junior",parent.getChildren().get(0).getName());
+        assertEquals("Houben",parent.getChildren().get(0).getName());
+        assertEquals("man",parent.getChildren().get(0).getName());
+        assertEquals(2,parent.getChildren().get(0).getName());
+
+
     }
 }
